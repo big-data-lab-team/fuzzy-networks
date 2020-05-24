480aaa4a3246dba01af0b29e9cd1edfa2076d1ff
Antoine Hébert <hantoine02@gmail.com>
2020-05-24 18:38:12 -0400

    Add notebook to evaluate the number of significant digits of probabilities predicted by the NumPy MLP on MNIST

--- mlp_numpy/evaluate.py
+++ mlp_numpy/evaluate.py
@@ -6,12 +6,20 @@ import uuid
 if len(sys.argv) < 2:
     print('The identifier of an experiment containing a trained neural network must be provided.')
     exit()
+if len(sys.argv) < 3:
+    docker_image_tag = 'no-mca'
+else:
+    docker_image_tag = sys.argv[2]
+experiment_dir = sys.argv[1]
 
-exp = utils.ExperimentResults(sys.argv[1])
+
+exp = utils.ExperimentResults(experiment_dir)
 nn = exp.load('neural_network')
 train, val, test = load_mnist()
 
 X, y = test
+X = X[:100] # Using all test examples would take too long
 y_pred_proba = nn.predict_proba(X)
 
-exp.save(y_pred_proba, f'test_predictions_{uuid.uuid4().hex}')
+result_name = f'test_predictions_{docker_image_tag}_{uuid.uuid4().hex}'
+exp.save(y_pred_proba, result_name)
--- mlp_numpy/predict_with_mca.sh
+++ mlp_numpy/predict_with_mca.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --rm --mount "type=bind,source=$(pwd),target=/workdir" -ti gkiar/fuzzy:python-numpy python3.6 evaluate.py $1
+docker run --rm --mount "type=bind,source=$(pwd),target=/workdir" -i gkiar/fuzzy:$2 python3.6 evaluate.py $1 $2
