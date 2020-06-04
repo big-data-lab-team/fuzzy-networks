651404e38a9d073c4ab3e9a9f7a8096c1a73320f
Antoine Hébert <hantoine02@gmail.com>
2020-06-04 15:39:18 -0400

    Merge main.py and evaluate.py so that we have only one script per architecture

--- mlp_numpy/predict_with_mca.sh
+++ mlp_numpy/predict_with_mca.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run --rm -e "VFC_BACKENDS=$3" --mount "type=bind,source=$(pwd),target=/workdir" -i gkiar/fuzzy:$2 python3.6 evaluate.py $1 "$2-$3"
+docker run --rm -e "VFC_BACKENDS=$3" --mount "type=bind,source=$(pwd),target=/workdir" -i gkiar/fuzzy:$2 python3.6 main.py evaluate $1 "$2-$3"
