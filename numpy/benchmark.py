9e12b5c6f2488d78f7e9d3e3bc5039c92e21f2c2
Ribal Aladeeb <aladeeb.r@hotmail.com>
2020-06-19 02:32:29 -0400

    Fix bug in benchmark.py

--- numpy/benchmark.py
+++ numpy/benchmark.py
@@ -4,5 +4,6 @@ from train_config import mlp_config, cnn_config
 epoch_run = [1, 10, 50, 100, 500,1000]
 
 for run in epoch_run:
+    mlp_config['hyperparameters']['n_epochs'] = run
     train.train(mlp_config)
-    print(f'\n\n\n*********Done training for {run} epochs')
+    print(f'\n\n\n*********Done training for {run} epochs*********\n\n\n')
